0175c12a44a4b3c863d7ae6247312eed99905802
Lucas <lucas@maple.finance>
2021-08-02 17:36:38 -0400

    fix: update unit testing, remove unecessary code

--- contracts/test/ERC2258.t.sol
+++ contracts/test/ERC2258.t.sol
@@ -2,82 +2,82 @@
 pragma solidity >=0.6.11;
 pragma experimental ABIEncoderV2;
 
-import { ERC2258 } from "../ERC2258.sol";
+import { IERC20 } from "../../modules/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { DSTest } from "../../modules/ds-test/src/test.sol";
-import { Custodian } from "./accounts/Custodian.sol";
+
+import { ERC2258 } from "../ERC2258.sol";
+
+import { Custodian }      from "./accounts/Custodian.sol";
 import { ERC2258Account } from "./accounts/ERC2258Account.sol";
 
-import { IERC20 } from "../../modules/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+contract MintableERC2258 is ERC2258 {
 
-interface Hevm {
+    constructor(string memory name, string memory symbol) ERC2258(name, symbol) public { }
 
-    function warp(uint256) external;
-    function store(address,bytes32,bytes32) external;
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
 
 }
 
+
 contract ERC2258Test is DSTest {
 
-    ERC2258     erc2258;
-    Custodian       cus;
-    Custodian      cus2;
-    ERC2258Account  act;
-    ERC2258Account act2;
-    Hevm           hevm;
-    
-
-    uint256 constant WAD = 10 ** 18;
-
-    constructor() public { hevm = Hevm(address(bytes20(uint160(uint256(keccak256("hevm cheat code")))))); }
+    MintableERC2258 token;
+    ERC2258Account  account;
+    Custodian       custodian1;
+    Custodian       custodian2;
 
     function setUp() public {
-        erc2258 = new ERC2258("Token", "TKN");
-        cus     = new Custodian();
-        cus2    = new Custodian();
-        act     = new ERC2258Account();
-        act2    = new ERC2258Account();
+        token      = new MintableERC2258("Token", "TKN");
+        account    = new ERC2258Account();
+        custodian1 = new Custodian();
+        custodian2 = new Custodian();
     }
 
     function test_increaseCustodyAllowance() public {
-        assertTrue(!act.try_erc2258_increaseCustodyAllowance(address(erc2258), address(cus), 1000 * WAD)); // act doesn't has the balance.
+        assertTrue(!account.try_erc2258_increaseCustodyAllowance(address(token), address(custodian1), 1));  // Account doesn't have any balance
 
-        mint(address(erc2258), address(act), 5000 * WAD);
-        assertTrue(act.try_erc2258_increaseCustodyAllowance(address(erc2258), address(cus), 1000 * WAD));
-        assertEq(erc2258.custodyAllowance(address(act), address(cus)), 1000 * WAD);
-        assertEq(erc2258.totalCustodyAllowance(address(act)), 1000 * WAD);
-        assertEq(erc2258.balanceOf(address(act)), 5000 * WAD);
+        token.mint(address(account), 500);
 
-        // Increase the custody allowance again.
-        assertTrue(act.try_erc2258_increaseCustodyAllowance(address(erc2258), address(cus2), 2000 * WAD));
-        assertEq(erc2258.custodyAllowance(address(act), address(cus2)), 2000 * WAD);
-        assertEq(erc2258.totalCustodyAllowance(address(act)), 3000 * WAD);
-        assertEq(erc2258.balanceOf(address(act)), 5000 * WAD);
+        assertEq(token.custodyAllowance(address(account), address(custodian1)), 0);
+        assertEq(token.totalCustodyAllowance(address(account)),                 0);
+        assertEq(token.balanceOf(address(account)),                             500);
+
+        assertTrue(account.try_erc2258_increaseCustodyAllowance(address(token), address(custodian1), 100));
+
+        assertEq(token.custodyAllowance(address(account), address(custodian1)), 100);
+        assertEq(token.custodyAllowance(address(account), address(custodian2)), 0);
+        assertEq(token.totalCustodyAllowance(address(account)),                 100);
+        assertEq(token.balanceOf(address(account)),                             500);
+
+        // Increase the custody allowance to another custodian
+        assertTrue(!account.try_erc2258_increaseCustodyAllowance(address(token), address(custodian2), 401));  // Account doesn't have enough balance
+        assertTrue( account.try_erc2258_increaseCustodyAllowance(address(token), address(custodian2), 400));
+
+        assertEq(token.custodyAllowance(address(account), address(custodian1)), 100);
+        assertEq(token.custodyAllowance(address(account), address(custodian2)), 400);
+        assertEq(token.totalCustodyAllowance(address(account)),                 500);
+        assertEq(token.balanceOf(address(account)),                             500);
     }
 
     function test_transferByCustodian() public {
-        mint(address(erc2258), address(act), 5000 * WAD);
-        assertTrue(act.try_erc2258_increaseCustodyAllowance(address(erc2258), address(cus), 1000 * WAD));
-        assertTrue(cus.try_erc2258_transferByCustodian(address(erc2258), address(act), address(act2), 1000 * WAD));
+        token.mint(address(account), 500);
 
-        assertEq(erc2258.balanceOf(address(act)),  5000 * WAD);
-        assertEq(erc2258.balanceOf(address(act2)), 0);
+        account.erc2258_increaseCustodyAllowance(address(token), address(custodian1), 100);
 
-        assertEq(erc2258.custodyAllowance(address(act), address(cus2)), 0);
-        assertEq(erc2258.totalCustodyAllowance(address(act)), 0);
+        assertEq(token.custodyAllowance(address(account), address(custodian1)), 100);
+        assertEq(token.custodyAllowance(address(account), address(custodian2)), 0);
+        assertEq(token.totalCustodyAllowance(address(account)),                 100);
+        assertEq(token.balanceOf(address(account)),                             500);
+
+        assertTrue(!custodian1.try_erc2258_transferByCustodian(address(token), address(account), address(custodian2), 101));
+        assertTrue( custodian1.try_erc2258_transferByCustodian(address(token), address(account), address(custodian2), 100));
+
+        assertEq(token.custodyAllowance(address(account), address(custodian1)), 0);
+        assertEq(token.custodyAllowance(address(account), address(custodian2)), 100);
+        assertEq(token.totalCustodyAllowance(address(account)),                 100);
+        assertEq(token.balanceOf(address(account)),                             500);
     }
 
-
-    // Manipulate mainnet ERC20 balance
-    function mint(address addr, address account, uint256 amt) public {
-        uint256 slot = 0;
-        uint256 bal  = IERC20(addr).balanceOf(account);
-
-        hevm.store(
-            addr,
-            keccak256(abi.encode(account, slot)), // Mint tokens
-            bytes32(bal + amt)
-        );
-
-        assertEq(IERC20(addr).balanceOf(account), bal + amt, "Balance slot is wrong");  // Assert new balance
-    }
 }
