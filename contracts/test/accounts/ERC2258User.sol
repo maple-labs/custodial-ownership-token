73b6b5e7ca3bd254764b01303fed45f17e38f351
Michael De Luca <michael.deluca@circle-free.com>
2021-09-01 10:13:08 -0400

    feat: Complete Test Accounts

--- contracts/test/accounts/ERC2258User.sol
+++ contracts/test/accounts/ERC2258User.sol
@@ -3,14 +3,22 @@ pragma solidity >=0.6.11;
 
 import { IERC2258 } from "../../interfaces/IERC2258.sol";
 
-contract ERC2258Account {
+contract ERC2258User {
+
+    /************************/
+    /*** Direct Functions ***/
+    /************************/
 
     function erc2258_increaseCustodyAllowance(address token, address custodian, uint256 amount) external {
         IERC2258(token).increaseCustodyAllowance(custodian, amount);
     }
 
+    /*********************/
+    /*** Try Functions ***/
+    /*********************/
+
     function try_erc2258_increaseCustodyAllowance(address token, address custodian, uint256 amount) external returns (bool ok) {
-        (ok,) = token.call(abi.encodeWithSignature("increaseCustodyAllowance(address,uint256)", custodian, amount));
+        (ok,) = token.call(abi.encodeWithSelector(IERC2258.increaseCustodyAllowance.selector, custodian, amount));
     }
 
 }
